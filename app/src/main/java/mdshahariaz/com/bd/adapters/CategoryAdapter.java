5406033ab2b81a454ee38a0bc5509dbb6c856f89
mdshahariazrimon <mriaz182048@gmail.com>
2022-11-14 15:28:11 +0600

    Category Adapter Class Created

---
+++ app/src/main/java/mdshahariaz/com/bd/adapters/CategoryAdapter.java
@@ -0,0 +1,15 @@
+package mdshahariaz.com.bd.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryAdapter {
+    //Create a subclass
+    public class CategoryViewHolder extends RecyclerView.ViewHolder{
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
